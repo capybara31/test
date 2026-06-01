6c181eb341e655f47fb44919f1310ea9648223eb
Kapibarik31 <bahsalievammar@gmail.com>
2026-06-01 17:27:58 +0500

    feat: add binary search algorithm implementation

--- algorithms.py
+++ algorithms.py
@@ -1,1 +1,14 @@
+def binary_search(array, target):
+    low = 0
+    high = len(array) - 1
 
+    while low <= high:
+        mid = (low + high) // 2
+        mid_item = array[mid]
+        if target == mid_item:
+            return mid
+        elif target > mid_item:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return None
